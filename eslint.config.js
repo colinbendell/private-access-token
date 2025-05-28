63a7fdb3a2b273ee868d75d22f85ec8421bcb766
Colin Bendell <colin@bendell.ca>
2025-05-28 15:17:07 -0400

    Update packages and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,27 @@
 import js from "@eslint/js";
+import jsdoc from 'eslint-plugin-jsdoc';
 
 export default [
     {
         ...js.configs.recommended,
+        ...jsdoc.configs['flat/recommended'],
         // "extends": [
         //   "eslint:recommended",
         //   "eslint-config-google"
         // ],
-        "rules": {
-            "indent": [ "error", 4 ],
+        plugins: {
+            jsdoc,
+        },
+        rules: {
+            indent: [ "error", 4 ],
             "brace-style": [ "error", "stroustrup" ],
-            "max-len": [ "error", 150 ],
-            "require-jsdoc": "error",
-            "valid-jsdoc": "error",
+            // "max-len": [ "error", 150 ],
+            "jsdoc/require-description": "error",
             "arrow-parens": [ "error", "as-needed" ]
         },
-        "languageOptions": {
-            "sourceType": "module",
-            "ecmaVersion": "latest",
+        languageOptions: {
+            sourceType: "module",
+            ecmaVersion: "latest",
         }
     }
 ]
